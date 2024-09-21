0749ace62d147ec8ac144039edffef425a3d87c5
maryus1991 <maryus19915123@gmail.com>
2024-09-21 16:05:55 +0330

    adding swagger and redocs for api

--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = 'Post Api',
+        default_version = 'v1',
+        description = 'the api of post part',
+
+    ),
+    public=True,
+    permission_classes = [permissions.AllowAny]
+
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('todo.urls')),
-    path('api/v1/', include('api_v1.urls'))
+    path('api/v1/', include('api_v1.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('swagger/output.json', schema_view.without_ui(cache_timeout=0)),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0))
 ]
